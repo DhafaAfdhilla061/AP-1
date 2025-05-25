0a6e18c256f0fcf8e5f1cd27807cf605d4cb673a
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:18:44 +0700

    materi1.cpp

--- PERTEMUAN 8/materi1.cpp
+++ PERTEMUAN 8/materi1.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-struct Mahasiswa{
-    string nama;
+struct Mahasiswa{// deklarasi struct 
+    string nama; // inisialisasi variabel yang termasuk dalam struct
     int umur;
     float ipk;
 };
 
 int main(){
-    Mahasiswa mhs1;
+    Mahasiswa mhs1; // inisialisasi variabel mhs1 yang bertipe Mahasiswa(struct yang dibuat berperan seperti tipe data) 
 
-    mhs1.nama = "Dhafa";
+    mhs1.nama = "Dhafa";// memberi nilai pada variabel mhs1 bagian nama di dalam struct nya 
     mhs1.umur = 19;
     mhs1.ipk=4.00;
 
@@ -20,8 +20,8 @@ int main(){
     // cout<<"Umur = "<<mhs1.umur<<endl;
     // cout<<"IPK  = "<<mhs1.ipk <<endl;
 
-    Mahasiswa *ptrMhs = &mhs1;
-    cout<<"Nama = "<<ptrMhs->nama<<endl;
+    Mahasiswa *ptrMhs = &mhs1; // kombinasi pointer dengan struct
+    cout<<"Nama = "<<ptrMhs->nama<<endl;// mengakses struct dengan pointer
     cout<<"Umur = "<<ptrMhs->umur<<endl;
     cout<<"IPK  = "<<ptrMhs->ipk<<endl;
 
