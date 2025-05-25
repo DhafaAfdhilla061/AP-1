030c25f1cf159085c6c0c91725fb038a3d0a2aba
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 20:45:31 +0700

    latihan.cpp

--- latihan.cpp
+++ latihan.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
-using namespace std;
+#include<iostream>// header untuk c++
+using namespace std;// untuk fungsi input output c++
 
-main(){
-    float v,lp,r;
+main(){// deklarasi fungsi utama program (({) untuk mengawali fungsi utama) 
+    float v,lp,r;// menginisialisasi variabel
 
-    cout<<"Masukkan jari-jari"; cin>>r;
+    cout<<"Masukkan jari-jari"; //menampilkan 
+    cin>>r;// meng-input data ke dalam variabel 
 
-    v=float(4)/3*3.14*r*r*r;
-    lp=4*3.14*r*r;
+    v=float(4)/3*3.14*r*r*r; //melakukan proses perhitungan volume ( (*)  adalah kali dan (/) adalah bagi) 
+    lp=4*3.14*r*r; // melakukan proses perhitungan luas permukaan
 
-    cout<<"Volume : "<<v<<endl;
-    cout<<"Luas permukaan  : "<<lp<<endl;
-}
+    cout<<"Volume : "<<v<<endl;// menampilkan hasil(volume) 
+    cout<<"Luas permukaan  : "<<lp<<endl;//menampilkan hasil (luas permukaan) 
+}((}) //untuk mengakhiri fungsi utama
