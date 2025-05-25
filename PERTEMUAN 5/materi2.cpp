b464ef386f3a4f6e6fda1680532b2cd1d2fe2f1a
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:01:30 +0700

    materi2.cpp

--- PERTEMUAN 5/materi2.cpp
+++ PERTEMUAN 5/materi2.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
-#include <vector>
+#include <vector>// header untuk menggunakan fungsi vektor
 using namespace std;
 
 int main(){
-    vector<string> nama_karyawan = {"Dapa","Ripki","Iyep","Parhan","Arkan","Wili","Frans","Keli"};
+    vector<string> nama_karyawan = {"Dapa","Ripki","Iyep","Parhan","Arkan","Wili","Frans","Keli"};// inisialisasi vektor dengan tipe string
     // for(string karyawan : nama_karyawan){
     //     cout<<karyawan<<endl;
     // }
 
     // Add data to Vector
-    nama_karyawan.push_back("Doni");
+    nama_karyawan.push_back("Doni");// menggunakan fungsi vektor yaitu pushback untuk menambah karakter di indeks paling akhir vektor 
 
     // for (int i=0;i<nama_karyawan.size();i++){
     //     cout<<nama_karyawan[i]<<endl;
     // }
     
     // Delete data from vector
-    nama_karyawan.pop_back();
+    nama_karyawan.pop_back();// menggunakan fungsi vektor popback untuk menghapus indeks paling akhir vektor
     // for (int i=0;i<nama_karyawan.size();i++){
     //     cout<<nama_karyawan[i]<<endl;
     // }
     
-    nama_karyawan.erase(nama_karyawan.begin()+3);
+    nama_karyawan.erase(nama_karyawan.begin()+3);// menghapus karakter di indeks ke-(0+3=3) 
     for (int i=0;i<nama_karyawan.size();i++){
         cout<<nama_karyawan[i]<<endl;
     }
