4d6fc4ae8af14079f3805a1acb221277b9adc5f8
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:09:56 +0700

    materi2.cpp

--- PERTEMUAN 6/materi2.cpp
+++ PERTEMUAN 6/materi2.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
-string namaGlobal = "Ilmu";
-void namaVariabel(){
-    string namaLokal = "Komputer";
+string namaGlobal = "Ilmu";// variabel global yang bisa digunakan baik di fungsi utama maupun fungsi lain
+void namaVariabel(){// deklarasi fungsi kosong tanpa parameter
+    string namaLokal = "Komputer";// variabel lokal yang hanya bisa digunakan di fungsi namaVariabel
     cout<<namaLokal<<endl;
     cout<<namaGlobal<<endl;
 }
 int main(){
-    namaVariabel();
+    namaVariabel();// memanggil fungsi namaVariabel
     cout<<namaGlobal<<endl;
     //cout<<namaLokal<<endl;
 }
