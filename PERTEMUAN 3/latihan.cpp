8694445c0fd90c43cf226453e4f2e36561d20348
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 21:26:08 +0700

    latihan.cpp

--- PERTEMUAN 3/latihan.cpp
+++ PERTEMUAN 3/latihan.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     int nilai;
     cout<<"masukkkan nilai"; cin>>nilai;
-    if (nilai%5==0 ){
-        cout<<"Habis dibagi 5";
-    } else if (nilai%10==0 ){
-        cout<<"Habis dibagi 10";
-    } else {
-        cout<<"Tidak habis dibagi keduanya";
+    if (nilai%5==0 ){//deklarasi pernyataan kondisi dengan kondisi nilai adalah kelipatan 5
+        cout<<"Habis dibagi 5";// pernyataan yang akan dieksekusi apabila nilai kondisi TRUE 
+    } else if (nilai%10==0 ){// deklarasi percabangan kondisi dengan kondisi nilai kelipatan 10
+        cout<<"Habis dibagi 10";// pernyataan yang akan dieksekusi apabila nilai kondisi TRUE 
+    } else {// deklarasi percangan kondisi apabila kondisi di atas bernilai FALSE 
+        cout<<"Tidak habis dibagi keduanya"; // pernyataan yang akan dieksekusi
     }
 }
