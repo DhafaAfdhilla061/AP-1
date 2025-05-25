4a753b8aae5501178cc83ca693741c15f5a5346a
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 21:48:16 +0700

    latihan3.cpp

--- PERTEMUAN 4/latihan3.cpp
+++ PERTEMUAN 4/latihan3.cpp
@@ -6,9 +6,9 @@ int main(){
     int i,j;
     cout<<"masukkan kode :";
     getline(cin,kode);
-    for(i=0;i<kode.length();i++){
+    for(i=0;i<kode.length();i++){// deklarasi perulangan for (inisialisasi i, kondisi i, dan perbaruan nilai i) 
         //cout<<kode[i];
-        for ( j=0;j<=i;j++){
+        for ( j=0;j<=i;j++){// deklarasi perulangan for di dalam for (inisialisasi J, kondisi j, dan perbaruan nilai J) 
             cout<<kode[j]; 
              
         }
