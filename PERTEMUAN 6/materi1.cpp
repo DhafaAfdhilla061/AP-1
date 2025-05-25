d1b9bb9d1088ad3e54a8ccc888720cf0c0c15165
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:07:59 +0700

    materi1.cpp

--- PERTEMUAN 6/materi1.cpp
+++ PERTEMUAN 6/materi1.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void sapa(string nama){
+void sapa(string nama){// deklarasi fungsi void(kosong) di mana fungsi tidak mengembalikan nilai dengan parameter formal string nama
     cout << "Halo "<<nama<<" ! Selamat belajar c++!"<<endl;
 }
 
 int main(){
     string namaPengguna = "Dhafa";
-    sapa(namaPengguna);
+    sapa(namaPengguna);// memanggil fungsi " sapa" dengan parameter aktual namaPengguna
 }
