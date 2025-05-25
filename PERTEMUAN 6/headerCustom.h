e95c1bd25068457ccd341e021ed42c489129ae6a
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:06:21 +0700

    headerCustom.h

--- PERTEMUAN 6/headerCustom.h
+++ PERTEMUAN 6/headerCustom.h
@@ -1,5 +1,6 @@
+// header buatan berupa defenisi untuk beberapa sintaks 
 #define u using namespace std;
-#define k <<
+#define k << // mengubah sintaks (<<) menjadi k
 #define o cout
 #define e endl;
 #define f system("CLS");
