757f514dde02386df8075619c9c59e44ed5c3435
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 22:22:05 +0700

    materi2.cpp

--- PERTEMUAN 8/materi2.cpp
+++ PERTEMUAN 8/materi2.cpp
@@ -3,8 +3,8 @@
 #include<vector>
 using namespace std;
 
-struct Alamat {
-    string jalan,kota;
+struct Alamat {// deklarasi struct 
+    string jalan,kota; // inisialisasi variabel dalam struct
     int kodepos;
 };
 
@@ -16,18 +16,18 @@ struct Mahasiswa{
 };
 
 int main(){
-    Mahasiswa mhs1;
-    // mhs1.nama = "Dhafa";
+    Mahasiswa mhs1;// inisialisasi variabel mhs1 bertipe struct Mahasiswa yang dibuat tadi
+    // mhs1.nama = "Dhafa";( menyimpan nilai pada variabel mhs1 bagian nama
     // mhs1.umur = 19;
     // mhs1.ipk=4.00;
 
-    // mhs1.alamat.jalan="Jalan Harapan Pasti Timur";
+    // mhs1.alamat.jalan="Jalan Harapan Pasti Timur"; ( menyimpan nilai pada variabel mhs1 bagian alamat lalu pada variabel alamat bagian jalan
     // mhs1.alamat.kota="Medan";
     // mhs1.alamat.kodepos=20228;
 
     // cout<<"Alamat = "<<mhs1.alamat.jalan<<" "<<mhs1.alamat.kota<<" "<<mhs1.alamat.kodepos<<endl;
     
-    vector<Mahasiswa> mahasiswa;
+    vector<Mahasiswa> mahasiswa;// inisialisasi vektor bertipe struct Mahasiwa
     int n;
     cout<<"Masukkan banyak mahasiswa : "; cin>>n;
 
@@ -36,13 +36,13 @@ int main(){
 
         cin.get();
         cout<<"Masukkan nama : ";
-        getline(cin,mhs1.nama);
+        getline(cin,mhs1.nama);// menyimpan nilai sementara di mhs1 
         cout<<"Masukkan umur : ";
         cin>> mhs1.umur;
         cout<<"Masukkan ipk : ";
         cin>> mhs1.ipk;
 
-        mahasiswa.push_back(mhs1);
+        mahasiswa.push_back(mhs1);// menambahkan nilai yang disimpan sementara di mhs1 ke vektor mahasiswa
     }
 
     for(int i=0;i<n;i++){
