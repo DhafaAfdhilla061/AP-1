8834a1edeb49822e01c75ae9c8fb141b09d2f7f5
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2025-05-25 21:46:27 +0700

    latihan2.cpp

--- PERTEMUAN 4/latihan2.cpp
+++ PERTEMUAN 4/latihan2.cpp
@@ -9,7 +9,7 @@ int main(){
     getline(cin, kalimat);
 
     //alya
-    for(i=0;i<kalimat.length();i++){
+    for(i=0;i<kalimat.length();i++){// deklarasi perulangan for (inisialisasi i untuk indikator perulangan, kondisi i, perbaruan i (i++)
         if (kalimat[i]=='a'||kalimat[i]=='i'||kalimat[i]=='u'||kalimat[i]=='e'||kalimat[i]=='o'){
             kalimat[i]= toupper(kalimat[i]);
         }
